2b0739c8e7b57b5a16239023b0098526492ddf13
baonguyen <bao678393@gmail.com>
2025-10-23 07:32:20 +0700

    video21

---
+++ 01-react-vite-typescript-starter-master/src/layout.tsx
@@ -0,0 +1,15 @@
+import TodoList from "./components/todo.list"
+
+
+function Layout() {
+  return (
+    <>
+    <div>
+      Hello word
+      <TodoList/>
+    </div>
+    </>
+  )
+}
+
+export default Layout
